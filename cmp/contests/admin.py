2b0721a97b3101600b9930345dc3ccd264c93d55
Rebbouh-Mohamed <info.moh.2003@gmail.com>
2024-12-18 19:48:43 +0100

    admin contest -sort particioant in admin panel

--- cmp/contests/admin.py
+++ cmp/contests/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class AdminContestParticipation(admin.ModelAdmin):
+    ordering=("-pionts",)
 
 admin.site.register(Contest)
-admin.site.register(ContestParticipation)
+admin.site.register(ContestParticipation,AdminContestParticipation)
